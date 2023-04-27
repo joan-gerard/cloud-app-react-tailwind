e755e43a18e105e35a7054b58cd1d5e5d2e57a19
Joan Gerard <joan.gerard@outlook.com>
2023-04-27 18:58:40 +0200

    add assets and build NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-4xl text-center text-red-500">Hello</h1>
+      <NavBar />
     </div>
   );
 }
