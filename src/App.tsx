98743c5504dafff5dfb0a9399636685cacc33731
Joan Gerard <joan.gerard@outlook.com>
2023-04-28 15:43:07 +0200

    write About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./components/About";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <NavBar />
       <Hero />
+      <About />
     </div>
   );
 }
