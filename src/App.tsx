6c08dbc5a3a5a5158b02efaf2ab703156de018dc
Joan Gerard <joan.gerard@outlook.com>
2023-04-28 19:40:40 +0200

    write Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "./components/About";
 import AllInOne from "./components/AllInOne";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Pricing from "./components/Pricing";
@@ -15,6 +16,7 @@ function App() {
       <Support />
       <AllInOne />
       <Pricing />
+      <Footer />
     </div>
   );
 }
